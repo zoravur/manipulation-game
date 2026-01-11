558513b9b99f6ba28b9389e0a52ef7ce44d65ce1
Giles Edkins <edkins@gmail.com>
2026-01-11 10:14:09 -0500

    Plot improvements

--- src/manipulation_game/plot.py
+++ src/manipulation_game/plot.py
@@ -5,6 +5,24 @@ import os
 import argparse
 
 from manipulation_game.experiment import ExperimentResult
+from manipulation_game.game import Game
+
+def summarize_games(games: dict[int,Game]) -> dict[int,str]:
+    include_scheming = len({g.a_scheming for g in games.values()}) > 1
+    include_commission = len({g.a_commission_percentage for g in games.values()}) > 1
+    include_model_pair = len({(g.a_model, g.b_model) for g in games.values()}) > 1
+    summaries = {}
+    for key, g in games.items():
+        # parts = [f"City: {g.realistic_city}"]
+        parts = []
+        if include_scheming:
+            parts.append(f"Scheming: {g.a_scheming}")
+        if include_commission:
+            parts.append(f"Commission: {g.a_commission_percentage}%")
+        if include_model_pair:
+            parts.append(f"A: {g.a_model} / B: {g.b_model}")
+        summaries[key] = ", ".join(parts)
+    return summaries
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -14,6 +32,7 @@ if __name__ == "__main__":
     # Load data from a file corresponding to the experiment name
     results_path = Path(__file__).parent.parent.parent / "results" / args.experiment
     counts = defaultdict(Counter)
+    games = {}
 
     with os.scandir(results_path) as entries:
         for entry in entries:
@@ -23,6 +42,9 @@ if __name__ == "__main__":
                     sub_id = data.game.sub_experiment_id
                     counts[(sub_id, "_rec")][data.results.recommended_restaurant] += 1
                     counts[(sub_id, "chosen")][data.results.chosen_restaurant] += 1
+                    games[sub_id] = data.game
+
+    summaries = summarize_games(games)
 
     values = set()
     for d in counts.values():
@@ -33,9 +55,10 @@ if __name__ == "__main__":
     for x, (key, d) in enumerate(sorted(counts.items())):
         bottom = 0
         for i, value in enumerate(values):
-            plt.bar(x, width=1, height=d[value], bottom=bottom, label=str(value) if x==0 else None, alpha=0.7, color=palette(i))
+            vstring = "unknown" if value == "" or value is None else str(value) 
+            plt.bar(x, width=1, height=d[value], bottom=bottom, label=vstring if x==0 else None, alpha=0.7, color=palette(i))
             bottom += d[value]
-    plt.xticks(range(len(counts)), [f"{k[0]} {'(rec)' if k[1]=='_rec' else '(chosen)'}" for k in sorted(counts.keys())], rotation=45, ha='right')
+    plt.xticks(range(len(counts)), [f"{summaries[k[0]]} {'(rec)' if k[1]=='_rec' else '(chosen)'}" for k in sorted(counts.keys())], rotation=45, ha='right')
     plt.legend()
     plt.tight_layout()
     plt.show()
